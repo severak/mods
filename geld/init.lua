21a097ef24f28c6631ac6f551365c5a5b9aa37a2
Hry <Hry>
2012-07-26 21:09:21 +0000

    Added other currencies.

--- geld/init.lua
+++ geld/init.lua
@@ -25,6 +25,48 @@ minetest.register_craftitem("geld:tetradrachm",{
 	inventory_image="geld_tetradrachm.png"
 })
 
+minetest.register_craftitem("geld:stela",{
+	description="Stela",
+	stack_max=100,
+	inventory_image="geld_stela.png"
+})
+
+minetest.register_craftitem("geld:huf",{
+	description="Forint",
+	stack_max=100,
+	inventory_image="geld_huf.png"
+})
+
+minetest.register_craftitem("geld:rupee",{
+	description="Rupee",
+	stack_max=100,
+	inventory_image="geld_rupee.png"
+})
+
+minetest.register_craftitem("geld:tolar",{
+	description="Tolar",
+	stack_max=100,
+	inventory_image="geld_tolar.png"
+})
+
+minetest.register_craftitem("geld:usd_5",{
+	description="5 $",
+	stack_max=100,
+	inventory_image="geld_usd_5.png"
+})
+
+minetest.register_craftitem("geld:with_hole",{
+	description="Money with hole",
+	stack_max=100,
+	inventory_image="geld_with_hole.png"
+})
+
+minetest.register_craftitem("geld:soviet_ruble",{
+	description="Soviet ruble",
+	stack_max=100,
+	inventory_image="geld_soviet_ruble.png"
+})
+
 minetest.register_on_newplayer(function(player)
 	local initial_geld=minetest.setting_get("give_initial_geld") or nil
 	if initial_geld then
